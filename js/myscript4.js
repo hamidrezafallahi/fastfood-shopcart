735b41ea52b932b1efb1940c85021fe297d0d241
hamidreza <hamidreza.lipar@gmail.com>
2023-06-14 11:09:58 +0330

    for resume

---
+++ js/myscript4.js
@@ -0,0 +1,439 @@
+data = `{
+    "product_groups": [
+          {
+              "group_id":1,
+              "group_title": "ساندویچ",
+              "group_image": "images/burger-7-310x260.png",
+              "group_products": [{
+                      "product_id":1,
+                      "product_name": "ژامبون مرغ تنوری",
+                      "product_type":["normal"],
+                      "product_price": [95000],
+                      "product_image": ["images/product-01-542x448.png",
+                                        "images/product-02-542x448.png",
+                                        "images/product-05-542x448.png"],
+                      "product_content": [
+                          "ژامبون مرغ",
+                          "قارچ",
+                          "پنیر",
+                          "سس مخصوص"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+
+                  },
+                  {
+                      "product_id":2,
+                      "product_name": "هات داگ مخصوص",
+                      "product_type":["normal"],
+                      "product_price": [100000],
+                      "product_image": ["images/product-01-542x448.png",
+                                        "images/product-02-542x448.png",
+                                        "images/product-05-542x448.png"],
+                      "product_content": [
+                          "هات داگ",
+                          "قارچ",
+                          "پنیر",
+                          "کاهو",
+                          "خیارشور",
+                          "گوجه"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+                  },
+                  {
+                      "product_id":3,
+                      "product_name": "چیز برگر",
+                      "product_type":["normal","double"],
+                      "product_price": [110000,155000],
+                      "product_image": ["images/product-01-542x448.png",
+                                        "images/product-02-542x448.png",
+                                        "images/product-05-542x448.png"],
+                      "product_content": [
+                          "همبرگر90 درصد",
+                          "پنیر گودا",
+                          "سس مخصوص",
+                          "گوجه"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+                  },
+                  {
+                      "product_id":4,                    
+                      "product_name": "رویال برگر",
+                      "product_type":["normal"],
+                      "product_price": [193000],
+                      "product_image": ["images/product-01-542x448.png",
+                                        "images/product-02-542x448.png",
+                                        "images/product-05-542x448.png"],
+                      "product_content": [
+                          "همبرگر 90 درصد",
+                          "ژامبون گوشت",
+                          "قارچ",
+                          "پنیر"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+                  }
+              ]
+          },
+          {
+              "group_id":2,
+              "group_title": "پیتزا",
+              "group_image": "images/pizza-7-310x260.png",
+              "group_products": [{
+                  "product_id":1,                
+                  "product_name": "پیتزا فیله مرغ",
+                  "product_type":["mini","normal","large"],
+                  "product_price": [110000,145000,210000],
+                  "product_image": ["images/product-01-542x448.png",
+                                    "images/product-02-542x448.png",
+                                    "images/product-05-542x448.png"],
+                  "product_content": [
+                      "فیله مرغ",
+                      "قارچ",
+                      "فلفل دلمه",
+                      "پنیر",
+                      "گوجه",
+                      "ذرت"
+                  ],
+                  "product_energy":32,
+                  "product_protein":79
+              }, {
+                  "product_id":2,                
+                  "product_name": "پیتزا پنجره ای مخصوص",
+                  "product_type":["mini","normal","large"],
+                  "product_price": [125000,185000,230000],
+                  "product_image": ["images/product-01-542x448.png",
+                                    "images/product-02-542x448.png",
+                                    "images/product-05-542x448.png"],
+                  "product_content": [
+                      "ژامبون بره",
+                      "قارچ",
+                      "پنیر",
+                      "گوشت چرخ کرده",
+                      "فلفل دلمه"
+                  ],
+                  "product_energy":45,
+                  "product_protein":83
+              }, {
+                  "product_id":3,                
+                  "product_name": "پیتزا پپرونی",
+                  "product_type":["mini","normal","large"],
+                  "product_price": [105000,135000,190000],
+                  "product_image": ["images/product-01-542x448.png",
+                                    "images/product-02-542x448.png",
+                                    "images/product-05-542x448.png"],
+                  "product_content": [
+                      "کالباس پپرونی",
+                      "قارچ",
+                      "پنیر",
+                      "فلفل دلمه"
+                  ],
+                  "product_energy":51,
+                  "product_protein":56
+              }]
+          },
+          {
+              "group_id":3,
+              "group_title": "کباب",
+              "group_image": "images/barbecue-7-310x260.png",
+              "group_products": 
+              [
+                  {
+                      "product_id":1,                
+                      "product_name": "کوبیده",
+                      "product_type":["single","double"],
+                      "product_price": [45000,85000],
+                      "product_image": ["images/product-01-542x448.png",
+                      "images/product-02-542x448.png",
+                      "images/product-05-542x448.png"],
+                      "product_content": [
+                          "گوشت چرخ کرده",
+                          "پیاز",
+                          "نمک",
+                          "جوش شیرین"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+                  }, 
+                  {
+                      "product_id":2,                
+                      "product_name": "برگ",
+                      "product_type":["normal"],
+                      "product_price": [130000],
+                      "product_image": ["images/product-01-542x448.png",
+                                        "images/product-02-542x448.png",
+                                        "images/product-05-542x448.png"],
+                      "product_content": [
+                          "گوشت گوسفندی",
+                          "نمک",
+                          "آبلیمو"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+                  }
+              ]
+          }
+ ,
+          {
+              "group_id":4,
+              "group_title": "سوشی",
+              "group_image": "images/barbecue-7-310x260.png",
+              "group_products": 
+              [
+                  {
+                      "product_id":1,                
+                      "product_name": "کوبیده",
+                      "product_type":["single","double"],
+                      "product_price": [45000,85000],
+                      "product_image": ["images/product-01-542x448.png",
+                      "images/product-02-542x448.png",
+                      "images/product-05-542x448.png"],
+                      "product_content": [
+                          "گوشت چرخ کرده",
+                          "پیاز",
+                          "نمک",
+                          "جوش شیرین"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+                  }, 
+                  {
+                      "product_id":2,                
+                      "product_name": "برگ",
+                      "product_type":["normal"],
+                      "product_price": [130000],
+                      "product_image": ["images/product-01-542x448.png",
+                                        "images/product-02-542x448.png",
+                                        "images/product-05-542x448.png"],
+                      "product_content": [
+                          "گوشت گوسفندی",
+                          "نمک",
+                          "آبلیمو"
+                      ],
+                      "product_energy":45,
+                      "product_protein":83
+                  }
+              ]
+          }         
+      ]
+  }`;
+
+//----------------------------------------------------------------
+discount_data = `{
+    "Discounts": [
+        { "group_id": 0, "group_title": "ساندویچ", "discount": 20 },
+        { "group_id": 1, "group_title": "پیتزا", "discount": 35 },
+        { "group_id": 2, "group_title": "کباب", "discount": 50 },
+        { "group_id": 3, "group_title": "نوشیدنی", "discount": 25 },
+        { "group_id": 4, "group_title": "سالاد", "discount": 65 },
+        { "group_id": 5, "group_title": "سوپ", "discount": 40 },
+        { "group_id": 6, "group_title": "دسر", "discount": 35 }
+    ]
+}`
+
+//----------------VARIABLE----------------------------------------------------------------------------------------------
+new_data = JSON.parse(discount_data);
+mydata = JSON.parse(data);
+var group_id=Number (location.search.substring(7,8))+1;
+var product_id=Number (location.search.substring(17))+1;
+var get =localStorage.getItem("purchase");  
+var js_get = JSON.parse(get)
+//----------------VARIABLE----------------------------------------------------------------------------------------------
+var i =js_get.length;
+var shopcard = document.getElementById("shopcart-count");
+shopcard.innerText=i;
+
+
+
+
+
+//----------------VARIABLE----------------------------------------------------------------------------------------------
+var table=document.getElementById("shopcart-table");
+var thead=document.createElement("thead");
+var tbody=document.createElement("tbody");
+function maketable() {
+    table.appendChild(tbody)
+    for (let index = 0; index < js_get.length; index++) {
+        const element = js_get[index];
+        var row = document.createElement("tr");
+        tbody.appendChild(row);
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+        var td33 = document.createElement("td");
+        var td4 = document.createElement("td");
+        var td5 = document.createElement("td");
+        var td6 = document.createElement("td");
+        var td7 = document.createElement("td");
+        var td8 = document.createElement("td");
+        var td9 = document.createElement("td");
+        var div = document.createElement('div');
+        var sp1 = document.createElement('span');
+        var sp2 = document.createElement('span');
+        var input = document.createElement('input');
+        var img = document.createElement('img');
+        var link = document.createElement('a');
+        var icon = document.createElement('i');
+        var quantity =Number(input.value);
+        var image = element.img;
+        var name = element.name;
+        var old_price = element.oldprice;
+        var discount =element.off;
+        var new_price =element.newprice;
+        var quantity =element.qty;
+        var type =element.type;
+        td1.setAttribute('class','td-1');
+        div.setAttribute('class','number');
+        td1.appendChild(div);
+        div.appendChild(sp1);
+        div.appendChild(input);
+        div.appendChild(sp2);
+        input.setAttribute('type','text');
+        input.setAttribute('id','number'+index);
+        input.setAttribute('value',element.qty);
+        input.setAttribute('data-zeros','true');
+        input.setAttribute('min',1);
+        input.setAttribute('max',20);
+        input.setAttribute('readonly','');
+        input.style.fontSize=("1em");
+        input.style.border="none";
+        input.style.width="20%";
+        input.style.textAlign="center";
+        input.style.margin="0 10px";
+        sp1.setAttribute('id',"plus_"+index)
+        sp2.setAttribute('id',"minus_"+index)
+
+        td2.setAttribute('class','td-2');
+        img.setAttribute('src',element.img);
+        img.setAttribute('alt',"");
+        img.setAttribute('width',"130");
+        img.setAttribute('height',"130");
+        td2.appendChild(img);
+        td3.setAttribute('class','td-3');
+        td3.innerText=element.name;
+        td33.setAttribute('class','td-5');
+        td33.innerText=element.type;
+        td4.setAttribute('class','td-4');
+        td4.innerText=element.oldprice+" تومان ";
+        td5.setAttribute('class','td-5');
+        td5.innerText=discount+"%";
+        td6.setAttribute('class','td-4');
+        td6.innerText=element.newprice+" تومان ";
+        td7.setAttribute('class','td-5');
+        td7.setAttribute('id','count'+[index]);
+        td7.innerText=element.qty+"عدد"
+        td8.style.width='20px';
+        
+        td8.setAttribute('id','td-8'+index);
+        td9.setAttribute('id','td-9'+index);
+        td9.style.width='150px';
+        icon.classList.add('fa');
+        icon.classList.add('fa-times');
+        link.appendChild(icon);
+        link.style.cursor="pointer";
+        td8.appendChild(link);
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+        row.appendChild(td33);
+        row.appendChild(td4);
+        row.appendChild(td5);
+        row.appendChild(td6);
+        row.appendChild(td7);
+        row.appendChild(td9);
+        row.appendChild(td8);
+        var row_sum = document.getElementById("td-9"+[index]);
+        var row_sum2 = quantity * (element.newprice);
+        row_sum.innerText=row_sum2+ " تومان"
+        var row_sum = document.getElementById("td-9"+[index]);
+        var row_sum2 = quantity * (element.newprice);
+        total()
+        sp1.addEventListener('click', ()=>{
+                        var row_sum = document.getElementById("td-9"+[index]);
+            var row_sum2 = quantity * (element.newprice);
+            row_sum.innerText=row_sum2+ " تومان"
+            var quantity =element.qty 
+            quantity+=1;
+            td7.innerText=quantity+"عدد"
+            js_get[index].qty = quantity;
+            var count = document.getElementById("count"+[index]);
+            var box_count = document.getElementById("number"+[index]);
+            count.innerText=quantity+" عدد";
+            box_count.value = quantity;
+            var string_request = JSON.stringify(js_get)
+            localStorage.setItem("purchase",string_request) 
+            console.log(element)
+            var row_sum = document.getElementById("td-9"+[index]);
+            var row_sum2 = quantity * (element.newprice);
+            row_sum.innerText=row_sum2+ " تومان"
+            total()
+        });
+        sp2.addEventListener('click', ()=>{
+            var quantity =element.qty 
+            if (quantity > 1 ) {
+                quantity --;
+                }
+            td7.innerText=quantity+"عدد"
+            js_get[index].qty = quantity;
+            var count = document.getElementById("count"+[index]);
+            var box_count = document.getElementById("number"+[index]);
+            count.innerText=quantity+" عدد"
+            box_count.value = quantity;
+            var string_request = JSON.stringify(js_get)
+            localStorage.setItem("purchase",string_request) 
+            var row_sum = document.getElementById("td-9"+[index]);
+            var row_sum2 = quantity * (element.newprice);
+            row_sum.innerText=row_sum2+ " تومان"
+            total()
+        }); 
+
+    }
+    row_sum.innerText=row_sum2+ " تومان";
+
+
+}
+
+//----------------FUNCTIONS--------------------------------------------------------------------------------------------
+function total() {
+    var temp = 0 ;
+    for (let h = 0; h < js_get.length; h++) {
+        const element1 = js_get[h];
+        element1.total = (element1.newprice)*(element1.qty);
+        temp +=element1.total
+        
+    }
+    var total = document.getElementById("total-price");
+    total.innerText = temp;
+}
+
+
+
+function removeFromMemory(){
+    var closes = document.getElementsByClassName('fa-times')
+    for (let index = 0; index < closes.length; index++) {
+        const element = closes[index];
+        element.addEventListener('click',()=>{console.log("hi : "+index)
+        js_get.splice(index, 1);
+        console.log(js_get)
+        var string_request = JSON.stringify(js_get)
+        localStorage.setItem("purchase",string_request) 
+        window.location.reload();
+        shopcart_counter();
+        })
+    
+    }
+}
+function shopcart_counter(){
+    var get =localStorage.getItem("purchase");  
+    var js_get = JSON.parse(get)
+    var i =js_get.length;
+    var shopcard = document.getElementById("shopcart-count");
+    shopcard.innerText=i;
+}
+
+maketable();
+removeFromMemory();
+shopcart_counter();
+
